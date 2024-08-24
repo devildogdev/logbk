62a58fe7a466d00c93361dca357bc371351a643c
j-tew <justinrtew@gmail.com>
2024-08-23 21:56:50 -0500

    Removing unneeded first steps

--- main.go
+++ main.go
@@ -7,21 +7,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-func getConfig() map[string]any {
-	v := viper.New()
-	v.SetDefault("journal_path", "~/journal")
-	v.SetConfigFile("./settings.json")
-	err := v.ReadInConfig()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return v.AllSettings()
-}
-
-func newEntry() {}
-
 func main() {
-	// conf := getConfig()
 	viper.SetDefault("journal_path", "~/journal")
 	viper.SetConfigFile("./settings.json")
 	err := viper.ReadInConfig()
