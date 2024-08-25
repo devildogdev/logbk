6209f7b6384731d34b7fbd69924b3cd805afbd53
j-tew <justinrtew@gmail.com>
2024-08-25 14:52:51 -0500

    open with nvim

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strconv"
 	"strings"
+	"syscall"
 	"time"
 
 	"github.com/spf13/viper"
@@ -25,13 +27,16 @@ func getConfig() *viper.Viper {
 	return app
 }
 
-func newEntryPath(root string) string {
+func newEntryPath(root string) (string, error) {
 	if strings.HasPrefix(root, "~") {
 		homeDir, err := os.UserHomeDir()
 		if err != nil {
 			log.Fatal(err)
 		}
 		root = filepath.Join(homeDir, filepath.Base(root))
+	}
+	if !filepath.IsAbs(root) {
+		return "", fmt.Errorf("%s is not an absolute path!", root)
 	}
 	now := time.Now()
 	y := strconv.Itoa(now.Year())
@@ -43,11 +48,30 @@ func newEntryPath(root string) string {
 	if len(d) < 2 {
 		d = fmt.Sprintf("0%s", d)
 	}
-	return fmt.Sprintf("%s/%s/%s/%s%s", root, y, m, d, ".md")
+	return fmt.Sprintf("%s/%s/%s/%s%s", root, y, m, d, ".md"), nil
+}
+
+func openFileInEditor(path string) error {
+	editor, ok := os.LookupEnv("EDITOR")
+	if !ok {
+		fmt.Println("$EDITOR not set. Using nvim.")
+		editor = "nvim"
+	}
+	bin, err := exec.LookPath(editor)
+	if err != nil {
+		return err
+	}
+	nvimArgs := []string{"nvim", path}
+	env := os.Environ()
+	syscall.Exec(bin, nvimArgs, env)
+	return nil
 }
 
 func main() {
 	conf := getConfig()
-	ep := newEntryPath(conf.GetString("journal_path"))
-	fmt.Println(ep)
+	ep, err := newEntryPath(conf.GetString("journal_path"))
+	if err != nil {
+		panic(err)
+	}
+	openFileInEditor(ep)
 }
